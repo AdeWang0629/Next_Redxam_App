307474673102acce0e191a6168f8b44983575538
AleJGD <alejandrogonzalezduarte@hotmail.com>
2021-11-26 10:21:25 -0400

    CardKYC

---
+++ packages/frontend/src/stories/dashboard/CardKYC.stories.tsx
@@ -0,0 +1,27 @@
+import React, { ComponentProps } from "react";
+
+import { Story, Meta } from "@storybook/react";
+
+import CardKYC from "@components/dashboard/CardKYC";
+
+import kycImage from "@public/images/kyc/kyc.svg";
+
+export default {
+  title: "dashboard/CardKYC",
+  component: CardKYC,
+} as Meta;
+
+const Template: Story<ComponentProps<typeof CardKYC>> = (args) => (
+  <div className="bg-black min-h-screen">
+    <CardKYC {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  cardImage: kycImage,
+  cardText:
+    "To continue adding a bank account to redxam you will need to complete your KYC verification. Which will be completed using sumsub.",
+  buttonText: "Start KYC Verification ",
+};
