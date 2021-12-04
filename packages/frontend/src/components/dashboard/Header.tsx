236800f57065db79b21e542c25c4f552cc477a77
AleJGD <alejandrogonzalezduarte@hotmail.com>
2021-12-04 11:22:22 -0400

    Header (#12)
    * Card reusable component
    
    * Header for the app
    
    * fix typescript
    
    Co-authored-by: Murtaja Ziad <mail@murtajaziad.xyz>

---
+++ packages/frontend/src/components/dashboard/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+import headerLogo from "@public/images/dashboard/header-logo.svg";
+
+const Header = () => {
+  return (
+    <div
+      className="w-full flex flex-col items-center justify-center h-40 space-y-5 rounded-b-3xl mb-10"
+      style={{
+        background: "linear-gradient(155.44deg, #9ef01a 0%, #38b000 100%)",
+      }}
+    >
+      <Image src={headerLogo} alt="redxam Logo" width="40px" height="35px" />
+    </div>
+  );
+};
+
+export default Header;
