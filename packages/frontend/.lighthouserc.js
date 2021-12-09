0ae9d61c042904515f86832b8f02598cf09bacb5
Murtaja Ziad <mail@murtajaziad.xyz>
2021-12-09 23:34:15 +0300

    feat(frontend): lighthouse action (#26)
    * feat(frontend): lighthouse action
    
    * feat(frontend): lighthouse action
    
    * feat(frontend): lighthouse action
    
    * feat(frontend): lighthouse action

---
+++ packages/frontend/.lighthouserc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'yarn start',
+      startServerReadyPattern: 'ready - started server on 0.0.0.0:3000, url: http://localhost:3000',
+      url: [
+        'http://localhost:3000/',
+        'http://localhost:3000/careers',
+        'http://localhost:3000/about',
+        'http://localhost:3000/blog',
+      ],
+      upload: {
+        target: 'temporary-public-storage',
+      },
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'uses-rel-preconnect': 'off',
+        'unused-javascript': 'off',
+        'tap-targets': 'warn',
+        'errors-in-console': 'warn',
+      },
+    },
+  },
+}
