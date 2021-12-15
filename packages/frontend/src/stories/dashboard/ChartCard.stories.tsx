494e4eb0cb00e76d2cea4e1b607ac58798865266
Murtaja Ziad <mail@murtajaziad.xyz>
2021-12-15 03:05:32 +0300

    feat(frontend): chart component (#27)
    * feat(frontend): chart component
    
    * feat(frontend): change chart component

---
+++ packages/frontend/src/stories/dashboard/ChartCard.stories.tsx
@@ -0,0 +1,30 @@
+import React, { ComponentProps } from "react";
+
+import { Story, Meta } from "@storybook/react";
+
+import Chart from "@components/dashboard/ChartCard";
+
+export default {
+  title: "Dashboard/ChartCard",
+  component: Chart,
+} as Meta;
+
+const Template: Story<ComponentProps<typeof Chart>> = (args) => {
+  return (
+    <>
+      <div className="h-[fit-content] bg-blue-400">
+        <Chart
+          {...args}
+          data={new Array(100).fill(0).map((_, i) => {
+            return {
+              time: new Date().getTime() + 60000 * 60 * 24 * i,
+              value: 100 * i - 3 * i * (Math.random() > 0.5 ? 1 : -1),
+            };
+          })}
+        />
+      </div>
+    </>
+  );
+};
+
+export const Default = Template.bind({});
