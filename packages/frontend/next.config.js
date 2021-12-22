1bf7b748599d49dfbdbf1a3de853c2c0c8047ef6
Murtaja Ziad <mail@murtajaziad.xyz>
2021-12-22 18:52:57 +0300

    add sentry (#42)

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const { withSentryConfig } = require("@sentry/nextjs");
+
+module.exports = withSentryConfig(
+  /** @type {import('next').NextConfig} */
+  {
+    reactStrictMode: true,
+  },
+  {
+    silent: true,
+  }
+);
