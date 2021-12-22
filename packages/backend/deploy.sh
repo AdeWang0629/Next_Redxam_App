3abefc6765d04d9448f485257ae62f27e7e9ec1e
JhosdryG <53978018+JhosdryG@users.noreply.github.com>
2021-12-22 03:09:25 -0400

    change env to production agaian

--- packages/backend/deploy.sh
+++ packages/backend/deploy.sh
@@ -4,12 +4,12 @@ npm install
 npm run build
 cp .env dist/.env
 pm2 delete all
-NODE_ENV="development" SERVICE="server" PORT=80 pm2 start dist/index.js --name server
-NODE_ENV="development" SERVICE="vaults" PORT=3001 pm2 start dist/index.js --name vaults
-NODE_ENV="development" SERVICE="wallets" PORT=3002 pm2 start dist/index.js --name wallets
-NODE_ENV="development" SERVICE="balance" PORT=3003 pm2 start dist/index.js --name balance
-NODE_ENV="development" SERVICE="portfolio" PORT=3004 pm2 start dist/index.js --name portfolio
-NODE_ENV="development" SERVICE="binance" PORT=3005 pm2 start dist/index.js --name binance
-NODE_ENV="development" SERVICE="webhooks" PORT=3006 pm2 start dist/index.js --name webhooks
+NODE_ENV="production" SERVICE="server" PORT=80 pm2 start dist/index.js --name server
+NODE_ENV="production" SERVICE="vaults" PORT=3001 pm2 start dist/index.js --name vaults
+NODE_ENV="production" SERVICE="wallets" PORT=3002 pm2 start dist/index.js --name wallets
+NODE_ENV="production" SERVICE="balance" PORT=3003 pm2 start dist/index.js --name balance
+NODE_ENV="production" SERVICE="portfolio" PORT=3004 pm2 start dist/index.js --name portfolio
+NODE_ENV="production" SERVICE="binance" PORT=3005 pm2 start dist/index.js --name binance
+NODE_ENV="production" SERVICE="webhooks" PORT=3006 pm2 start dist/index.js --name webhooks
 pm2 save
 # EOF
