33b9f10b836b9cc22d347636a3af7b5c95a5e90b
SoftwareAndOutsourcing <5988222+SoftwareAndOutsourcing@users.noreply.github.com>
2022-01-04 07:44:28 +0100

    Fix ufw setup

--- setup-scripts/linux.sh
+++ setup-scripts/linux.sh
@@ -12,7 +12,7 @@
 # Enables ports 80 & 3006
 sudo apt install ufw
 sudo ufw default deny incoming
-sudo ufw default deny outgoing
+sudo ufw default allow outgoing
 sudo ufw allow OpenSSH
 sudo ufw allow 80
 sudo ufw allow 3006
