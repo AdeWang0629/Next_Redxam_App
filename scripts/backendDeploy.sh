97bdc30fe7c4b6cee218613bcce9a76717c6a604
JhosdryG <53978018+JhosdryG@users.noreply.github.com>
2022-01-04 18:32:10 -0400

    Deployscript (#87)
    * fix attachments routes
    
    * remove reinstallation from deploy scripts
    
    * remove commented code in script

--- scripts/backendDeploy.sh
+++ scripts/backendDeploy.sh
@@ -4,19 +4,6 @@
 # ENVIRONMENT can be development, staging or production
 ENVIRONMENT=$1
 
-# install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-
-# install lts node version
-nvm install --lts
-npm i -g yarn
-npm i -g pm2 
-
-# Clone repo
-# git clone https://user:token@github.com/redxam/redxamapp.git
 cd redxamapp
 if [ "$ENVIRONMENT" = "development" ]; then
     git checkout develop
@@ -27,7 +14,6 @@ else
     git checkout main
 fi
 git pull
-# git remote set-url origin https://user:token@github.com/redxam/redxamapp.git
 
 # installing packages
 cd packages/backend
