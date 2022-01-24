912a65d35b6413a17dfde81e414b22693d4df035
SoftwareAndOutsourcing <5988222+SoftwareAndOutsourcing@users.noreply.github.com>
2022-01-24 12:14:30 +0100

    Set domain-specific locale

--- packages/frontend/next-i18next.config.js
+++ packages/frontend/next-i18next.config.js
@@ -2,5 +2,11 @@ module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es", "ar"],
-  },
+    domains: [
+      {
+        domain: 'redxam.ae',
+        defaultLocale: 'ar'
+      }
+    ]
+  }
 };
