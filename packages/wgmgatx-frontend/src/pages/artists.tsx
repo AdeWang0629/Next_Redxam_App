c188ec14a2a0f70fe4200cfbf51b707afea0c7f7
AleJGD <alejandrogonzalezduarte@hotmail.com>
2022-01-25 22:19:57 -0400

    Upcoming and artists

--- packages/wgmgatx-frontend/src/pages/artists.tsx
+++ packages/wgmgatx-frontend/src/pages/artists.tsx
@@ -1,12 +1,22 @@
-import ArtistList from "@components/artists/ArtistList";
-import Navbar from "@components/general/Navbar";
 import { NextPage } from "next";
+import Image from "next/image";
+import Navbar from "@components/general/Navbar";
+import ArtistList from "@components/artists/ArtistList";
+import wgmgArtistsImg from "@public/images/artists/artists-wgmg.jpeg";
 
 const Artists: NextPage = () => {
   return (
     <>
       <Navbar title="Artists" />
-      <ArtistList />
+      <div className="flex justify-center py-20">
+        <Image
+          src={wgmgArtistsImg || ""}
+          alt="WGMG Artists IMG"
+          width="1080px"
+          height="1080px"
+        />
+      </div>
+      {/* <ArtistList /> */}
     </>
   );
 };
