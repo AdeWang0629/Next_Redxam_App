f1ff02343a216f3cb8bf051576816a08a063f461
JhosdryG <53978018+JhosdryG@users.noreply.github.com>
2022-01-27 19:58:24 -0400

    create updateReferral script

--- packages/backend/src/resolver/admin.resolver/index.ts
+++ packages/backend/src/resolver/admin.resolver/index.ts
@@ -2,10 +2,12 @@ import { adminLogin } from './adminLogin.resolver';
 import { admin } from './admin.resolver';
 import { createAdmin } from './createAdminResolver';
 import { overview } from './overview.resolver';
+import { updateReferral } from './updateReferral.resolver';
 
 export const AdminResolver = {
   adminLogin,
   admin,
   createAdmin,
   overview,
+  updateReferral,
 };
