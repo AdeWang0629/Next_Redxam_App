da3c284ffb57205dc335320bb696f6bb325fd856
JhosdryG <53978018+JhosdryG@users.noreply.github.com>
2022-01-31 22:47:47 -0400

    updateUserStatus resolver

---
+++ packages/backend/src/resolver/admin.resolver/updateUserStatus.resolver.ts
@@ -0,0 +1,96 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { render } from 'mustache';
+import { verify } from 'jsonwebtoken';
+import { Request } from 'express';
+import { Attachment } from 'nodemailer/lib/mailer';
+import { Admin, User, UserProps } from '@/database';
+import { sendMail } from '@/apis/sendgrid';
+import { generateCode } from '@/utils/helpers';
+import getAuthorizationToken from '../getAuthorizationToken';
+
+const { SERVICE_EMAIL, TOKEN_SECURITY_KEY } = process.env;
+
+interface adminToken {
+  adminId: string;
+}
+
+export const updateUserStatus = async ({ arg }: { arg: string }, req: Request) => {
+  console.debug('[Resolve] admin called');
+  const auth = getAuthorizationToken(req.headers.authorization);
+  if (!auth.success) return { message: auth.message, success: auth.success };
+
+  try {
+    const payload = verify(auth.token, TOKEN_SECURITY_KEY) as adminToken;
+
+    const adminData = await Admin.findOne({ _id: payload.adminId });
+    if (!adminData) return { success: false, message: 'invalid admin token' };
+
+    const user = await User.findOne({ email: arg });
+    if (!user) return { success: false, message: 'email is not in the waitlist' };
+    if (user.accountStatus === 'invited')
+      return { success: false, message: 'user is already invited' };
+
+    await handleChangeAccountStatus(user);
+
+    return { success: true, message: 'user status updated succesfully' };
+  } catch (err) {
+    return { message: err.message, success: false };
+  }
+};
+
+const handleChangeAccountStatus = async (user: UserProps) => {
+  await user.updateOne({ $set: { accountStatus: 'invited' } });
+  await handleEmail(user);
+};
+
+const handleEmail = async (user: UserProps) => {
+  await sendMail({
+    from: `redxam.com <${SERVICE_EMAIL}>`,
+    to: user.email,
+    subject: 'You got access 🎉 | redxam',
+    html: render(templateData, {
+      code: generateCode(user.email),
+      randomText: `Ref #: ${Date.now()}`,
+    }),
+    attachments: [facebookIcon, twitterIcon, linkedInIcon, telegramIcon, discordIcon],
+  });
+};
+
+const templatePath = resolve(__dirname, '../../emails/invited.hjs');
+const templateData = readFileSync(templatePath, 'utf-8');
+
+const facebookIcon: Readonly<Attachment> = Object.freeze({
+  filename: 'facebook.png',
+  content: readFileSync(`${__dirname}/../../emails/facebook.png`).toString('base64'),
+  content_id: 'facebook@invited',
+  disposition: 'inline',
+});
+
+const twitterIcon: Readonly<Attachment> = Object.freeze({
+  filename: 'twitter.png',
+  content: readFileSync(`${__dirname}/../../emails/twitter.png`).toString('base64'),
+  content_id: 'twitter@invited',
+  disposition: 'inline',
+});
+
+const linkedInIcon: Readonly<Attachment> = Object.freeze({
+  filename: 'linkedin.png',
+  content: readFileSync(`${__dirname}/../../emails/linkedin.png`).toString('base64'),
+  content_id: 'linkedin@invited',
+  disposition: 'inline',
+});
+
+const telegramIcon: Readonly<Attachment> = Object.freeze({
+  filename: 'telegram.png',
+  content: readFileSync(`${__dirname}/../../emails/telegram.png`).toString('base64'),
+  content_id: 'telegram@invited',
+  disposition: 'inline',
+});
+
+const discordIcon: Readonly<Attachment> = Object.freeze({
+  filename: 'discord.png',
+  content: readFileSync(`${__dirname}/../../emails/discord.png`).toString('base64'),
+  content_id: 'discord@invited',
+  disposition: 'inline',
+});
