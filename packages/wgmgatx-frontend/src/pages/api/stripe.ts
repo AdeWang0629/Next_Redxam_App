821da806bf78a64b5f7357ebc0d8ec7c985aa2e1
AleJGD <alejandrogonzalezduarte@hotmail.com>
2022-02-03 19:47:06 -0400

    Update stripe.ts

--- packages/wgmgatx-frontend/src/pages/api/stripe.ts
+++ packages/wgmgatx-frontend/src/pages/api/stripe.ts
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('./stripe')(process.env.STRIPE_SECRET_KEY);
 
 async function CreateStripeSession(
   req: { body: { item: any } },
