945155947affbfda3eb9bd28929f2cab08fb2667
JhosdryG <53978018+JhosdryG@users.noreply.github.com>
2022-02-07 14:01:56 -0400

    sanitize and shallow equal helper

--- packages/backend/src/utils/helpers.test.ts
+++ packages/backend/src/utils/helpers.test.ts
@@ -1,4 +1,4 @@
-import { generateCode, isValidEmail } from './helpers';
+import { generateCode, isValidEmail, sanitize, shallowCompare } from './helpers';
 
 test('generateCode returns an string', () => {
   expect(typeof generateCode('sample@email.com')).toBe('string');
@@ -7,6 +7,33 @@ test('generateCode returns an string', () => {
 test('wrong email', () => {
   expect(isValidEmail('gfd.fdg.@gmail..com')).toBeFalsy();
 });
+
 test('valid email', () => {
   expect(isValidEmail('test@redxam.com')).toBeTruthy();
+});
+
+test('falsy shallow compare', () => {
+  const a = { a: 'a', b: 'b' };
+  const b = { a: 'b', b: 'a' };
+  expect(shallowCompare(a, b)).toBeFalsy();
+});
+
+test('Truthy shallow compare', () => {
+  const a = { a: 'a', b: 'b' };
+  const b = { a: 'a', b: 'b' };
+  expect(shallowCompare(a, b)).toBeTruthy();
+});
+
+test('sanitize object', () => {
+  const inputs = {
+    name: '<b><a href="https://www.crypto.com">link</a><b>',
+    lastName: '<pre>text<pre>',
+  };
+  const expectedInpunt = {
+    name: 'link',
+    lastName: 'text',
+  };
+  const res = sanitize(inputs);
+
+  expect(shallowCompare(res, expectedInpunt)).toBeTruthy();
 });
--- packages/backend/src/utils/helpers.ts
+++ packages/backend/src/utils/helpers.ts
@@ -26,3 +26,15 @@ export const isValidEmail = (email: string): boolean =>
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
     email,
   );
+
+export const sanitize = <T>(inputs: T): T => {
+  let sanitizedInpunts: T = {} as T;
+  Object.keys(inputs).forEach(key => {
+    sanitizedInpunts[key] = inputs[key].replace(/(<([^>]+)>)/gi, '');
+  });
+  return sanitizedInpunts;
+};
+
+export const shallowCompare = (obj1, obj2) =>
+  Object.keys(obj1).length === Object.keys(obj2).length &&
+  Object.keys(obj1).every(key => obj2.hasOwnProperty(key) && obj1[key] === obj2[key]);
