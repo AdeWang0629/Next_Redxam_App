89175b074f5f07ba93d51af4da108f974ec61afa
Murtaja Ziad <mail@murtajaziad.xyz>
2022-02-16 00:15:57 +0300

    chore: change arabic font

--- packages/frontend/src/pages/_document.tsx
+++ packages/frontend/src/pages/_document.tsx
@@ -3,24 +3,24 @@ import Document, {
   Head,
   Main,
   NextScript,
-  DocumentContext,
-} from "next/document";
+  DocumentContext
+} from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps, locale: ctx?.locale || "es" };
+    return { ...initialProps, locale: ctx?.locale || 'es' };
   }
 
   render() {
     return (
       <Html
-        dir={this.props.locale === "ar" ? "rtl" : "ltr"}
+        dir={this.props.locale === 'ar' ? 'rtl' : 'ltr'}
         lang={this.props.locale}
       >
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Rubik:wght@400;500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Rubik:wght@400;500;600;700&family=Tajawal:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
