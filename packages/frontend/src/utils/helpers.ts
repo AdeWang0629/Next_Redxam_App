52d360db3931a5ba7c70fb18c7cf919acda2fd00
Murtaja Ziad <mail@murtajaziad.xyz>
2022-02-16 22:09:41 +0300

    add raised from previous investors

--- packages/frontend/src/utils/helpers.ts
+++ packages/frontend/src/utils/helpers.ts
@@ -1,12 +1,17 @@
 function validateEmail(email: string) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
 function getMonthName(month: number) {
-  return new Date(2000, month - 1, 1).toLocaleString("default", {
-    month: "long",
+  return new Date(2000, month - 1, 1).toLocaleString('default', {
+    month: 'long'
   });
 }
 
-export { validateEmail, getMonthName };
+function numberWithCommas(x: number) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+export { validateEmail, getMonthName, numberWithCommas };
