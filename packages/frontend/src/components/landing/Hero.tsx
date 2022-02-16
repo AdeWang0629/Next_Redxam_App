1e7c917ac05dfa1f8cdbcd219a370b774dea877b
AleJGD <alejandrogonzalezduarte@hotmail.com>
2022-02-16 14:30:46 -0400

    Event banner with countdown

--- packages/frontend/src/components/landing/Hero.tsx
+++ packages/frontend/src/components/landing/Hero.tsx
@@ -24,28 +24,28 @@ const Hero: NextComponentType = () => {
 
   return (
     <>
-      <section className='max-w-7xl px-16 md:px-0 mx-auto flex flex-col items-center mt-40 mb-72'>
-        <h1 className='text-5xl md:text-7xl leading-tight mb-10 text-center font-bold text-lighter-black dark:text-gray-200 capitalize'>
+      <section className="max-w-7xl px-16 md:px-0 mx-auto flex flex-col items-center mt-24 mb-72">
+        <h1 className="text-5xl md:text-7xl leading-tight mb-10 text-center font-bold text-lighter-black dark:text-gray-200 capitalize">
           {t('title-first') + ' '}
-          <span className='relative before:transform before:scale-x-0 before:origin-bottom-right before:block before:absolute before:inset-0 before:bg-[#38f53b] before:z-[-1] before:transition-transform before:duration-300 before:ease-linear hover:before:scale-x-100 hover:before:origin-bottom-left'>
+          <span className="relative before:transform before:scale-x-0 before:origin-bottom-right before:block before:absolute before:inset-0 before:bg-[#38f53b] before:z-[-1] before:transition-transform before:duration-300 before:ease-linear hover:before:scale-x-100 hover:before:origin-bottom-left">
             {t('title-second-span')}
           </span>{' '}
           {t('title-third')}
         </h1>
-        <p className='font-primary text-[1.0625rem] max-w-[43.0625rem] mb-[1.875rem] text-center text-black dark:text-white text-opacity-80'>
+        <p className="font-primary text-[1.0625rem] max-w-[43.0625rem] mb-[1.875rem] text-center text-black dark:text-white text-opacity-80">
           {t('description')}
         </p>
         <button
-          className='font-primary text-[15px] w-[15rem] py-3.5 font-bold text-center rounded-[30px] mb-10 bg-buttons-green'
+          className="font-primary text-[15px] w-[15rem] py-3.5 font-bold text-center rounded-[30px] mb-10 bg-buttons-green"
           onClick={() => setWaitlistModelOpened(true)}
-          id='join-waiting'
+          id="join-waiting"
         >
           {t('join-button')}
         </button>
-        <Link href='/#benefits' shallow scroll>
-          <Image src={MouseIcon} alt='scroll' />
+        <Link href="/#benefits" shallow scroll>
+          <Image src={MouseIcon} alt="scroll" />
         </Link>
-        <Image src={HeroImage} alt='screenshots from the app' />
+        <Image src={HeroImage} alt="screenshots from the app" />
       </section>
       {waitlistModelOpened && (
         <WaitlistModel
