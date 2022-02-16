6a8083cfaf22cb8ee1fdde2bd2fd9b24b12f76ac
Diego Vielma <69025127+dandrevielma@users.noreply.github.com>
2022-02-16 14:16:57 -0400

    Merge branch 'develop' of https://github.com/redxam/redxamapp into develop
    Adding styles to timer

--- packages/frontend/src/components/landing/EventBanner.tsx
+++ packages/frontend/src/components/landing/EventBanner.tsx
@@ -26,18 +26,51 @@ const EventBanner: NextComponentType = () => {
     <>
       <section className="w-1/2 px-16 md:px-0 mx-auto flex justify-center items-center mt-60">
         <div
-          className="w-[70%] p-5 uppercase font-primary font-black text-[30px] py-3.5 font-bold text-center rounded-full mb-10 bg-buttons-green"
+          className="w-[70%] p-5 uppercase font-primary font-black text-[24px] font-bold text-center rounded-full bg-buttons-green mr-6"
           style={{ fontFamily: 'Montserrat' }}
         >
           1pm podcast on clubhose!!
         </div>
-        <div className="flex">
-          <span className="mx-2">0</span>
-          <span className="mx-2">2</span>
-          <span className="mx-2">32</span>
-          <span className="mx-2">16</span>
+        <div className='text-center flex flex-col items-center justify-center'>
+          <div className="flex font-black" style={{ fontFamily: 'Montserrat' }}>
+            <div className="mx-2 flex flex-col">
+              <span className='text-2xl'>
+                0
+              </span>
+              <span className='text-sm'>
+                DAYS
+              </span>
+            </div>
+            <div className="mx-2 flex flex-col">
+              <span className='text-2xl'>
+                2
+              </span>
+              <span className='text-sm'>
+                HOURS
+              </span>
+            </div>
+            <div className="mx-2 flex flex-col">
+              <span className='text-2xl'>
+                23
+              </span>
+              <span className='text-sm'>
+                MINUTES
+              </span>
+            </div>
+            <div className="mx-2 flex flex-col">
+              <span className='text-2xl'>
+                14
+              </span>
+              <span className='text-sm'>
+                SECONDS
+              </span>
+            </div>
+          </div>
         </div>
       </section>
+      <div className='text-center text-2xl font-bold text-[#000] mt-6 hover:text-[#ACE96B] ease-in-out duration-150'>
+        <a href='#'>- CLICK HERE TO JOIN -</a>
+      </div>
     </>
   );
 };
