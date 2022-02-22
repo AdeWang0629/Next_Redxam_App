4904f7d860e586f9b1efa38406c82571c37605fc
Diego Vielma <69025127+dandrevielma@users.noreply.github.com>
2022-02-22 14:12:16 -0400

    Mobile Menu Fixed

--- packages/wgmgatx-frontend/src/components/home/Hero.tsx
+++ packages/wgmgatx-frontend/src/components/home/Hero.tsx
@@ -7,12 +7,12 @@ import heroMobile from '@public/mobile_wgmg.jpg';
 const Hero = () => {
   return (
     <>
-      <div className="bg-[#171717] rounded-[18px]">
+      <div className="bg-[#171717] rounded-[18px] z-0">
         <div className='h-[35vh]'>
           <div className='relative w-full h-full md:grid md:grid-cols-2'>
-            <div className='z-50 absolute md:static h-full flex flex-col items-start md:items-end justify-center p-10 md:p-5'>
+            <div className='z-10 absolute md:static h-full flex flex-col items-start md:items-end justify-center p-10 md:p-5'>
               <h1 className='text-2xl md:text-2xl xl:text-3xl 2xl:text-5xl font-black md:text-right' style={{textShadow: '1px 1px 6px rgba(0, 0, 0, 1)'}}>
-              The Best Place to get your Physical and Digital Assets
+                The Best Place to get your Physical and Digital Assets
               </h1>
               <h4 className='z-50 text-md 2xl:text-2xl text-left md:text-right mt-5' style={{textShadow: '1px 1px 6px rgba(0, 0, 0, 1)'}}>
                 <i>We want to provide you a good oportunity to get art easy and secure</i>
