f62aff38bccf33cd2c938829c53c09468ee33837
AleJGD <alejandrogonzalezduarte@hotmail.com>
2022-03-15 19:23:08 -0400

    translation for signup

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -24,7 +24,7 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
     props: {
       ...(await serverSideTranslations(locale, [
         'navbar',
-        'waitlist',
+        'signup',
         'login',
         'hero',
         'whyus',
