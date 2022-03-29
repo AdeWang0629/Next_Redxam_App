c88eaa13374c773b856f218f167dcb46b6edba40
AleJGD <alejandrogonzalezduarte@hotmail.com>
2022-03-29 13:31:38 -0400

    Fix translation for login to say Signup and fix modals in ios

--- packages/frontend/src/components/models/SignupModel.tsx
+++ packages/frontend/src/components/models/SignupModel.tsx
@@ -62,98 +62,104 @@ const SignupModel: NextPage<SignupModelProps> = ({ isOpened, setOpened }) => {
   };
 
   return (
-    <div
-      className="flex flex-col justify-center items-center bg-black bg-opacity-75 absolute top-0 left-0 h-[100dvh] h-full w-full z-50"
-      ref={outsideContainerRef}
-      onClick={handleOutsideClick}
-      role="dialog"
-    >
-      <div className="flex flex-col items-center bg-white rounded-[30px] w-3/4 lg:w-1/2 xl:w-2/5 2xl:w-1/3  px-6 py-16 relative overflow-hidden">
-        <div
-          className={`bg-buttons-green w-full py-3 absolute top-0 rounded-t-[30px] transition-transform duration-500 ${
-            !signupSuccess && 'translate-x-[-1000px]'
-          }`}
-        >
-          <p className="text-black text-center font-medium font-secondary">
-            {t('success')}
-          </p>
-        </div>
-        <div
-          className={`bg-[#ae2727] w-full py-3 absolute top-0 rounded-t-[30px] transition-transform duration-500 ${
-            !error && 'translate-x-[-1000px]'
-          }`}
-        >
-          <p className="text-black text-center font-medium font-secondary">
-            {t('error')}
-          </p>
-        </div>
-        <h3 className="mb-2.5 text-4xl text-black text-opacity-80 text-center">
-          {signupSuccess ? t('inbox') : t('join')}
-        </h3>
-        <p className="w-full mb-5 text-black text-opacity-80 leading-[1.8] text-lg font-primary text-center">
-          {t('signup')}
-        </p>
-
-        {emailInvalid && (
-          <p className="w-full text-[#ae2727] text-opacity-80 leading-[1.8] text-md font-primary text-center font-bold">
-            {t('invalid-email')}
-          </p>
-        )}
-
-        <form className="flex flex-col" onSubmit={handleSubmit}>
-          <div className="flex flex-col md:flex-row mt-5">
-            <div className="md:mr-2 input-wrapper">
-              <input
-                type="text"
-                className="font-secondary"
-                onChange={e => setFirstName(e.target.value)}
-                value={firstName}
-                id="firstName"
-                disabled={signupSuccess}
-              />
-              <label className="font-primary" htmlFor="firstName">
-                {t('first-name')}
-              </label>
-            </div>
-            <div className="md:ml-2 mt-5 md:mt-0 input-wrapper">
-              <input
-                type="text"
-                className="font-secondary"
-                onChange={e => setLastName(e.target.value)}
-                id="lastName"
-                value={lastName}
-                disabled={signupSuccess}
-              />
-              <label className="font-primary" htmlFor="lastName">
-                {t('second-name')}
-              </label>
-            </div>
+    <div className="bg-black bg-opacity-75 absolute top-0 left-0 h-[200%] w-full z-50">
+      <div
+        className="flex flex-col justify-center items-center h-screen"
+        ref={outsideContainerRef}
+        onClick={handleOutsideClick}
+        role="dialog"
+      >
+        <div className="flex flex-col items-center bg-white rounded-[30px] w-3/4 lg:w-1/2 xl:w-2/5 2xl:w-1/3  px-6 py-16 relative overflow-hidden">
+          <div
+            className={`bg-buttons-green w-full py-3 absolute top-0 rounded-t-[30px] transition-transform duration-500 ${
+              !signupSuccess && 'translate-x-[-1000px]'
+            }`}
+          >
+            <p className="text-black text-center font-medium font-secondary">
+              {t('success')}
+            </p>
           </div>
           <div
-            className={`mt-5 input-wrapper w-full ${emailInvalid && 'invalid'}`}
+            className={`bg-[#ae2727] w-full py-3 absolute top-0 rounded-t-[30px] transition-transform duration-500 ${
+              !error && 'translate-x-[-1000px]'
+            }`}
           >
-            <input
-              type="text"
-              className="font-secondary w-full"
-              onChange={e => setEmail(e.target.value.toLowerCase())}
-              onBlur={() => checkEmail()}
-              value={email}
-              id="email"
-              disabled={signupSuccess}
-            />
-            <label className="font-primary" htmlFor="email">
-              {t('email')}
-            </label>
+            <p className="text-black text-center font-medium font-secondary">
+              {t('error')}
+            </p>
           </div>
-
-          <button
-            className="text-lighter-black font-primary font-medium text-lg leading-[20px] tracking-[-0.02em] bg-buttons-green rounded-[30px] mt-8 py-4 px-8 outline-none cursor-pointer transition-opacity duration-300 hover:opacity-70 disabled:opacity-30 disabled:cursor-not-allowed"
-            disabled={loading || emailInvalid || email === '' || signupSuccess}
-            id="join-waiting-button"
-          >
+          <h3 className="pt-6 lg:pt-0 mb-2.5 text-4xl text-black text-opacity-80 text-center">
             {signupSuccess ? t('inbox') : t('join')}
-          </button>
-        </form>
+          </h3>
+          <p className="w-full mb-5 text-black text-opacity-80 leading-[1.8] text-lg font-primary text-center">
+            {t('signup')}
+          </p>
+
+          {emailInvalid && (
+            <p className="w-full text-[#ae2727] text-opacity-80 leading-[1.8] text-md font-primary text-center font-bold">
+              {t('invalid-email')}
+            </p>
+          )}
+
+          <form className="flex flex-col" onSubmit={handleSubmit}>
+            <div className="flex flex-col md:flex-row mt-5">
+              <div className="md:mr-2 input-wrapper">
+                <input
+                  type="text"
+                  className="font-secondary"
+                  onChange={e => setFirstName(e.target.value)}
+                  value={firstName}
+                  id="firstName"
+                  disabled={signupSuccess}
+                />
+                <label className="font-primary" htmlFor="firstName">
+                  {t('first-name')}
+                </label>
+              </div>
+              <div className="md:ml-2 mt-5 md:mt-0 input-wrapper">
+                <input
+                  type="text"
+                  className="font-secondary"
+                  onChange={e => setLastName(e.target.value)}
+                  id="lastName"
+                  value={lastName}
+                  disabled={signupSuccess}
+                />
+                <label className="font-primary" htmlFor="lastName">
+                  {t('second-name')}
+                </label>
+              </div>
+            </div>
+            <div
+              className={`mt-5 input-wrapper w-full ${
+                emailInvalid && 'invalid'
+              }`}
+            >
+              <input
+                type="text"
+                className="font-secondary w-full"
+                onChange={e => setEmail(e.target.value.toLowerCase())}
+                onBlur={() => checkEmail()}
+                value={email}
+                id="email"
+                disabled={signupSuccess}
+              />
+              <label className="font-primary" htmlFor="email">
+                {t('email')}
+              </label>
+            </div>
+
+            <button
+              className="text-lighter-black font-primary font-medium text-lg leading-[20px] tracking-[-0.02em] bg-buttons-green rounded-[30px] mt-8 py-4 px-8 outline-none cursor-pointer transition-opacity duration-300 hover:opacity-70 disabled:opacity-30 disabled:cursor-not-allowed"
+              disabled={
+                loading || emailInvalid || email === '' || signupSuccess
+              }
+              id="join-waiting-button"
+            >
+              {signupSuccess ? t('inbox') : t('join')}
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
