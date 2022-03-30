7d6b06b38b6e0a2ff6126bbfe3a879bbe581400c
AleJGD <alejandrogonzalezduarte@hotmail.com>
2022-03-30 13:19:45 -0400

    Fix bg for modals

--- packages/frontend/src/components/models/LoginModel.tsx
+++ packages/frontend/src/components/models/LoginModel.tsx
@@ -63,75 +63,77 @@ const LoginModel: NextPage<LoginModelProps> = ({ isOpened, setOpened }) => {
 
   return (
     <>
-      <div
-        className="flex flex-col justify-center items-center bg-black bg-opacity-75 absolute top-0 left-0 h-[100dvh] h-full w-full z-50"
-        ref={outsideContainerRef}
-        onClick={handleOutsideClick}
-        role="dialog"
-      >
-        <div className="flex flex-col items-center bg-white rounded-[30px] w-[90%] md:w-1/2 xl:w-2/5 2xl:w-1/4 px-6 py-12">
-          {!submitted ? (
-            <>
-              <h3 className="mb-2.5 text-4xl text-black text-opacity-80 text-center">
-                {t('login-title')}
-              </h3>
-              <p className="w-full mb-5text-black text-opacity-80 leading-[1.8] text-lg font-primary text-center">
-                {t('login-desc')}
-              </p>
-              <form className="flex flex-col" onSubmit={handleSubmit}>
-                <div className="mt-5 input-wrapper w-full">
-                  <input
-                    type="email"
-                    className="font-secondary w-full"
-                    onChange={e => setEmail(e.target.value)}
-                    value={email}
-                    required
-                    id="email"
-                  />
-                  <label className="font-primary" htmlFor="email">
-                    {t('email-placeholder')}
-                  </label>
-                </div>
-                <button
-                  className="text-white font-primary font-medium text-lg leading-[20px] tracking-[-0.02em] bg-[#27AE60] rounded-[30px] mt-8 py-4 px-8 outline-none cursor-pointer transition-opacity duration-300 hover:opacity-70 disabled:opacity-30 disabled:cursor-not-allowed"
-                  disabled={loading}
-                >
-                  {t('login-button')}
-                </button>
-              </form>
-            </>
-          ) : response ? (
-            <div className="flex flex-col">
-              <h3 className="mb-2.5 text-4xl text-black text-opacity-80 text-center">
-                {t('sent')}
-              </h3>
-              <p className="w-full mb-5text-black text-opacity-80 leading-[1.8] text-lg font-primary text-center">
-                {t('sent-desc')}
-              </p>
-            </div>
-          ) : (
-            <div className="flex flex-col">
-              <h3 className="mb-2.5 text-4xl text-black text-opacity-80 text-center">
-                {t('innactive')}
-              </h3>
-              <p className="w-full mb-5text-black text-opacity-80 leading-[1.8] text-lg font-primary text-center">
-                {t('innactive-desc')}
-              </p>
-              <div className="flex flex-col justify-center items-center">
-                <button
-                  className="font-primary text-[15px] px-16 py-4 font-bold text-center rounded-[30px] bg-buttons-green order-first md:order-none mt-[25px] md:mt-0"
-                  onClick={() => setSignupOpened(true)}
-                >
-                  {t('join-waitlist')}
-                </button>
-                <a href="mailto:hello@redxam.com">
-                  <button className="font-primary text-[15px] px-16 py-4 font-bold text-center rounded-[30px] bg-buttons-green order-first md:order-none mt-2">
-                    {t('email-us')}
+      <div className="bg-black bg-opacity-75 absolute top-0 left-0 h-[200%] w-full z-50">
+        <div
+          className="flex flex-col justify-center items-center h-screen"
+          ref={outsideContainerRef}
+          onClick={handleOutsideClick}
+          role="dialog"
+        >
+          <div className="flex flex-col items-center bg-white rounded-[30px] w-[90%] md:w-1/2 xl:w-2/5 2xl:w-1/4 px-6 py-12">
+            {!submitted ? (
+              <>
+                <h3 className="mb-2.5 text-4xl text-black text-opacity-80 text-center">
+                  {t('login-title')}
+                </h3>
+                <p className="w-full mb-5text-black text-opacity-80 leading-[1.8] text-lg font-primary text-center">
+                  {t('login-desc')}
+                </p>
+                <form className="flex flex-col" onSubmit={handleSubmit}>
+                  <div className="mt-5 input-wrapper w-full">
+                    <input
+                      type="email"
+                      className="font-secondary w-full"
+                      onChange={e => setEmail(e.target.value)}
+                      value={email}
+                      required
+                      id="email"
+                    />
+                    <label className="font-primary" htmlFor="email">
+                      {t('email-placeholder')}
+                    </label>
+                  </div>
+                  <button
+                    className="text-white font-primary font-medium text-lg leading-[20px] tracking-[-0.02em] bg-[#27AE60] rounded-[30px] mt-8 py-4 px-8 outline-none cursor-pointer transition-opacity duration-300 hover:opacity-70 disabled:opacity-30 disabled:cursor-not-allowed"
+                    disabled={loading}
+                  >
+                    {t('login-button')}
                   </button>
-                </a>
+                </form>
+              </>
+            ) : response ? (
+              <div className="flex flex-col">
+                <h3 className="mb-2.5 text-4xl text-black text-opacity-80 text-center">
+                  {t('sent')}
+                </h3>
+                <p className="w-full mb-5text-black text-opacity-80 leading-[1.8] text-lg font-primary text-center">
+                  {t('sent-desc')}
+                </p>
               </div>
-            </div>
-          )}
+            ) : (
+              <div className="flex flex-col">
+                <h3 className="mb-2.5 text-4xl text-black text-opacity-80 text-center">
+                  {t('innactive')}
+                </h3>
+                <p className="w-full mb-5text-black text-opacity-80 leading-[1.8] text-lg font-primary text-center">
+                  {t('innactive-desc')}
+                </p>
+                <div className="flex flex-col justify-center items-center">
+                  <button
+                    className="font-primary text-[15px] px-16 py-4 font-bold text-center rounded-[30px] bg-buttons-green order-first md:order-none mt-[25px] md:mt-0"
+                    onClick={() => setSignupOpened(true)}
+                  >
+                    {t('join-waitlist')}
+                  </button>
+                  <a href="mailto:hello@redxam.com">
+                    <button className="font-primary text-[15px] px-16 py-4 font-bold text-center rounded-[30px] bg-buttons-green order-first md:order-none mt-2">
+                      {t('email-us')}
+                    </button>
+                  </a>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
       {signupOpened && (
