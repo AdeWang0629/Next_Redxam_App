e2075212aeb878f7f6a88257d8beb71ae12296fa
AleJGD <alejandrogonzalezduarte@hotmail.com>
2022-04-01 18:09:26 -0400

    api for getting medium feed

--- packages/frontend/src/utils/api.ts
+++ packages/frontend/src/utils/api.ts
@@ -679,6 +679,12 @@ class API {
       }
     );
   }
+
+  getMediumBlogs() {
+    return this.axios.get(
+      'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@redxam'
+    );
+  }
 }
 
 export default new API();
