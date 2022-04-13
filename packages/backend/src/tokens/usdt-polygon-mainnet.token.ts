6f4551b74534df5352a08d77e492052e5426f0fa
JhosdryG <53978018+JhosdryG@users.noreply.github.com>
2022-04-13 14:51:49 -0400

    matic prod

--- packages/backend/src/tokens/usdt-polygon-mainnet.token.ts
+++ packages/backend/src/tokens/usdt-polygon-mainnet.token.ts
@@ -36,6 +36,9 @@ import {
   REDXAM_ADDRESS_MATIC
 } from './consts';
 
+import TEST_ABI from './usdt-polygon-abi/usdt-polygon-testnet.abi.json';
+import ABI from './usdt-polygon-abi/usdt-polygon-mainnet.abi.json';
+
 export class MATICMainnetToken implements Token {
   isPendingDeposit(status: DepositStatus, deposit: DepositsProps): boolean {
     return false;
@@ -48,15 +51,6 @@ export class MATICMainnetToken implements Token {
     deposit: DepositsProps
   ): boolean {
     return false;
-  }
-  getUnspentInfo(txs: [], wallet: Wallet) {
-    return null;
-  }
-  handleThreshold(unspentInfo: UnspentInfo, wallet: Wallet): Promise<void> {
-    return new Promise((resolutionFunc, _) => resolutionFunc(null));
-  }
-  createRawTx(txData: TxData, unspentInfo: UnspentInfo): { hash: string } {
-    return { hash: 'asd' };
   }
   readonly name = 'Polygon';
   readonly symbol = 'MATIC';
@@ -104,14 +98,15 @@ export class MATICMainnetToken implements Token {
   }
 
   getWalletDeposits(txs: TransactionMatic[], address: string): Deposit[] {
-    return txs.map((tx, index) => {
-      if (tx.address.toLowerCase() !== address.toLowerCase()) return null;
-      return {
-        ...tx,
-        address,
-        index
-      };
-    });
+    return txs
+      .filter(tx => tx.address.toLowerCase() === address.toLowerCase())
+      .map((tx, index) => {
+        return {
+          ...tx,
+          address,
+          index
+        };
+      });
   }
 
   hasWalletNewTxs(wallet: Wallet, txs: Deposit[]): boolean {
@@ -191,6 +186,95 @@ export class MATICMainnetToken implements Token {
       return error.message;
     }
   }
+  async handleTokenThreshold(wallet: Wallet): Promise<void> {
+    const balance = await this.getBalance(wallet.address);
+
+    if (balance >= this.threshold) {
+      await this.sendToFee(wallet);
+      const rpc = this.isTestNet
+        ? 'https://rpc-mumbai.matic.today'
+        : 'https://polygon-rpc.com';
+      const contract_address = this.isTestNet
+        ? '0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1'
+        : '0xc2132D05D31c914a87C6611C10748AEb04B58e8F';
+      const decimals = this.isTestNet ? 18 : 6;
+      const contractABI = this.isTestNet ? TEST_ABI : ABI;
+
+      const amount = balance.toString();
+
+      const gas_limit = '0x100000';
+
+      const ethersProvider = new ethers.providers.JsonRpcProvider(rpc);
+      const walletCon = new ethers.Wallet(wallet.wif, ethersProvider);
+
+      ethersProvider.getGasPrice().then(currentGasPrice => {
+        // @ts-ignore
+        let gas_price = ethers.utils.hexlify(parseInt(currentGasPrice));
+        // general token send
+        let contract = new ethers.Contract(
+          contract_address,
+          contractABI,
+          walletCon
+        );
+
+        let numberOfTokens = ethers.utils.parseUnits(amount, decimals);
+        console.log(`numberOfTokens: ${numberOfTokens}`);
+
+        contract
+          .transfer(this.redxamAddress, numberOfTokens, {
+            gasLimit: ethers.utils.hexlify(gas_limit),
+            gasPrice: gas_price
+          })
+          .then(transferResult => {
+            console.dir(transferResult);
+          })
+          .catch(console.log);
+      });
+    }
+  }
+
+  async sendToFee(wallet: Wallet): Promise<void> {
+    const rpc = this.isTestNet
+      ? 'https://rpc-mumbai.matic.today'
+      : 'https://polygon-rpc.com/';
+
+    const sender_address = this.isTestNet
+      ? '0xF623BFF5733Dd50178C595a433E1956d2747c144'
+      : '0xF623BFF5733Dd50178C595a433E1956d2747c144';
+    // change for a prod redxam wallet in the future
+
+    const senderPrivateKey = this.isTestNet
+      ? '25cfcd4dee586c955459b2f27f6a615091ebcafb1366563b35cd2e5b02454172'
+      : '25cfcd4dee586c955459b2f27f6a615091ebcafb1366563b35cd2e5b02454172';
+
+    const amount = '0.001';
+
+    const gas_limit = '0x100000';
+
+    const ethersProvider = new ethers.providers.JsonRpcProvider(rpc);
+    const walletCon = new ethers.Wallet(senderPrivateKey);
+    const walletSigner = walletCon.connect(ethersProvider);
+
+    ethersProvider.getGasPrice().then(currentGasPrice => {
+      // @ts-ignore
+      let gas_price = ethers.utils.hexlify(parseInt(currentGasPrice));
+      console.log(`gas_price: ${gas_price}`);
+      const tx = {
+        from: sender_address,
+        to: wallet.address,
+        value: ethers.utils.parseEther(amount),
+        nonce: ethersProvider.getTransactionCount(sender_address, 'latest'),
+        gasLimit: ethers.utils.hexlify(gas_limit),
+        gasPrice: gas_price
+      };
+      try {
+        walletSigner.sendTransaction(tx).then(transaction => {});
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
+
   async tokenToFiat(wey: number, fiat: Fiats): Promise<number> {
     const decimals = this.isTestNet ? 18 : 6;
     return wey / Math.pow(10, decimals);
