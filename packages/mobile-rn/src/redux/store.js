196116f8ab794d936c5208dd5d459bf70bfd4ecd
JhosdryG <53978018+JhosdryG@users.noreply.github.com>
2022-04-25 16:50:47 -0400

    set up redux

--- packages/mobile-rn/src/redux/store.js
+++ packages/mobile-rn/src/redux/store.js
@@ -1,4 +1,23 @@
-import { createStore } from "redux";
-import { firstReducer } from "./reducers";
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-export const store = createStore(firstReducer) ;
+// envs
+import {NODE_ENV} from 'react-native-dotenv';
+
+const initialState = {};
+const middleware = [thunk];
+
+// Production
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    NODE_ENV === 'development' &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
+);
+
+export default store;
