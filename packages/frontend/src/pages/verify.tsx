52592bb1e5247ab37eb2d72c7ea506033e35b72b
JhosdryG <53978018+JhosdryG@users.noreply.github.com>
2022-04-27 14:13:13 -0400

    oldlogin

--- packages/frontend/src/pages/verify.tsx
+++ packages/frontend/src/pages/verify.tsx
@@ -13,32 +13,30 @@ import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useContext, useEffect } from 'react';
 import { UserContext } from '@providers/User';
+import { setCookies } from 'cookies-next';
 import api from 'src/utils/api';
 import Loader from '@components/global/Loader';
-import { io } from 'socket.io-client';
-import { getCookie } from 'cookies-next';
 
 const Verify: NextPage = () => {
-  const { setUser, setLoading, setNoUser, loading } = useContext(UserContext);
+  const { setUser, setLoading, setNoUser } = useContext(UserContext);
   const router = useRouter();
 
   useEffect(() => {
-    if (!router.query.token || !router.query.email) return;
+    if (!router.query.token) return;
 
     setLoading(true);
     api
       .verify(router.query.token as string)
       .then(async ({ data }) => {
         if (data.data.verifyToken.success) {
-          const { token } = data.data.verifyToken;
-          const { email } = router.query;
-          const socket = io(
-            (getCookie('environment') &&
-            getCookie('environment') === 'development'
-              ? process.env.NEXT_PUBLIC_DEV_BASE_URL
-              : process.env.NEXT_PUBLIC_PROD_BASE_URL) as string
-          );
-          socket.emit('onVerified', { token, email });
+          setCookies('token', data.data.verifyToken.token);
+          api.getUserData().then(({ data: userData }) => {
+            setUser(userData.data.user[0]);
+            setNoUser(false);
+            if (userData.data.user[0].accountStatus === 'invited') {
+              router.push('/invite');
+            } else router.push('/home');
+          });
         } else {
           alert(data.data.verifyToken.message);
           router.push('/login');
@@ -52,9 +50,7 @@ const Verify: NextPage = () => {
         setLoading(false);
       });
   }, [router.query?.token, router, setLoading, setNoUser, setUser]);
-
-  if (loading) return <Loader height="h-screen" />;
-  return <h1>You can now close this screen</h1>;
+  return <Loader height="h-screen" />;
 };
 
 export default Verify;
