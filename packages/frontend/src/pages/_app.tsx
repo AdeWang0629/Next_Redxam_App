f5b13fb10fd1b7431825327cb340e3b9a503a8a1
Max Awad <max@redxam.com>
2022-07-30 18:00:22 -0500

    updated tarra

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-script-component-in-head */
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
 import EnvironmentsSwitcher from '@components/global/EnvironmentsSwitcher';
@@ -10,6 +11,7 @@ import { appWithTranslation } from 'next-i18next';
 import { useLocale } from '@utils/hooks';
 import 'tippy.js/dist/tippy.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const locale = useLocale();
@@ -120,6 +122,20 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#58be05" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#58be05" />
+        <Script
+          strategy="afterInteractive"
+          id="tiktok"
+          dangerouslySetInnerHTML={{
+            __html: `
+          !function (w, d, t) {
+            w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
+            ttq.load('CB2UJS3C77U3T6AMG1V0');
+            ttq.page();
+            ttq.track('ViewContent');
+          }(window, document, 'ttq');      
+        `
+          }}
+        />
       </Head>
 
       {process.env.NODE_ENV !== 'production' ? <EnvironmentsSwitcher /> : ''}
