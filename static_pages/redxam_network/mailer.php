72f7d91ab488240c2a7063835b1d0d2640f0ab72
Max Awad <max@redxam.com>
2022-09-08 15:05:59 -0500

    pushing all updates

--- static_pages/redxam_network/mailer.php
+++ static_pages/redxam_network/mailer.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "support@devsdesign.net";
+        $recipient = "hello@redxam.network";
 
         // Set the email subject.
         $subject = "New contact from $name";
